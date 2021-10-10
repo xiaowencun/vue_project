bebe024cfe510c1996d9cc8bb41084e794dbeee7
xiaowencun <2484040983@qq.com>
2021-10-10 21:01:08 +0800

    登录功能提交

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
+import './assets/css/global.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+Vue.prototype.$http = axios
+
+
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from "../components/Login.vue"
+import Home from "../components/Home.vue"
+
+
 
 Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  },
+  {
+    path: '/home',
+    component: Home
+  }
+]
+let router = new Router({
+  routes,
+  mode: 'history'
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path == '/login') return next()
+  let tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
